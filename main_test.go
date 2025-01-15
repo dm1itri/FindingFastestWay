b73575e9b54acd492a2f8525390a4769a1468321
dimma-18 <Dimma-18@yandex.ru>
2025-01-16 01:05:38 +0300

    testing FindingFastestWay and correction of error in the calculations of the way

--- main_test.go
+++ main_test.go
@@ -20,6 +20,13 @@ func EqualMatrix(t *testing.T, actual, expected [][]int) {
 	}
 }
 
+func EqualSlice[T comparable](t *testing.T, actual, expected []T) {
+	t.Helper()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got: %v; want: %v", actual, expected)
+	}
+}
+
 func TestCorrectPoint(t *testing.T) {
 	matrix := [][]int{{1, 2}, {0, 4}}
 	tests := []struct {
@@ -110,6 +117,59 @@ func TestReadInput(t *testing.T) {
 			Equal(t, finish, tt.finish)
 			if err != nil {
 				Equal(t, err.Error(), tt.err)
+			}
+		})
+	}
+}
+
+func TestFindingFastestWay(t *testing.T) {
+	tests := []struct {
+		name      string
+		matrix    [][]int
+		start     Point
+		finish    Point
+		wantWay   []Point
+		wantError string
+	}{
+		{
+			name:      "Easy way",
+			matrix:    [][]int{{5, 2}, {3, 3}},
+			start:     Point{0, 0},
+			finish:    Point{1, 1},
+			wantWay:   []Point{{1, 1}, {1, 0}, {0, 0}},
+			wantError: "",
+		},
+		{
+			name:      "Way with wall",
+			matrix:    [][]int{{1, 1, 9}, {2, 0, 1}, {1, 4, 1}, {1, 1, 1}},
+			start:     Point{0, 0},
+			finish:    Point{2, 1},
+			wantWay:   []Point{{2, 1}, {2, 2}, {2, 3}, {1, 3}, {0, 3}, {0, 2}, {0, 1}, {0, 0}},
+			wantError: "",
+		},
+		{
+			name:      "One way with walls",
+			matrix:    [][]int{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}},
+			start:     Point{0, 0},
+			finish:    Point{2, 2},
+			wantWay:   []Point{{2, 2}, {2, 1}, {2, 0}, {3, 0}, {4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0}},
+			wantError: "",
+		},
+		{
+			name:      "Way not found",
+			matrix:    [][]int{{1, 1, 1}, {1, 0, 1}, {0, 9, 0}},
+			start:     Point{0, 0},
+			finish:    Point{1, 2},
+			wantWay:   nil,
+			wantError: "the way was not found",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			way, err := FindingFastestWay(tt.matrix, tt.start, tt.finish)
+			EqualSlice(t, way, tt.wantWay)
+			if err != nil {
+				Equal(t, err.Error(), tt.wantError)
 			}
 		})
 	}
